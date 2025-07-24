24aa9f571b76526d6d0d3f6ebd9273d967ebce23
Franco Novoa <franconovoa3@gmail.com>
2025-07-23 21:43:34 -0500

    Extend and collapse sidebar logic

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,40 +1,44 @@
-import React from 'react';
-import './Sidebar.css'; 
-import {assets} from '../../assets/assets';
+import React, { useState } from "react";
+import "./Sidebar.css";
+import { assets } from "../../assets/assets";
 
 const Sidebar = () => {
-    return (
-        <div className="sidebar">
-            <div className="top">
-                <img className="menu" src={assets.menu_icon} alt="Menu" />
-                <div className="new-chat">
-                    <img src={assets.plus_icon} alt="Plus Icon" />
-                    <p>New Chat</p>
-                </div>
-                <div className="recent">
-                    <p className="recent-title">Recent</p>
-                    <div className="recent-entry">
-                        <img src={assets.message_icon} alt="Message Icon" />
-                        <p>Sample query... </p>
-                    </div>
-                </div>
-            </div>
-            <div className="bottom">
-                <div className="bottom-item recent-entry">
-                    <img src={assets.question_icon} alt="Question" />
-                    <p>Help</p>
-                </div>
-                <div className="bottom-item recent-entry">
-                    <img src={assets.history_icon} alt="History" />
-                    <p>Activity</p>
-                </div>
-                <div className="bottom-item recent-entry">
-                    <img src={assets.settings_icon} alt="Settings" />
-                    <p>Settings</p>
-                </div>
-            </div>
+  const [extended, setExtended] = React.useState(false);
+
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <img onClick={() => setExtended(!extended)} className="menu" src={assets.menu_icon} alt="Menu" />
+        <div className="new-chat">
+          <img src={assets.plus_icon} alt="Plus Icon" />
+          {extended ? <p>New Chat</p> : null}
         </div>
-    );
-}
+        {extended ? (
+          <div className="recent">
+            <p className="recent-title">Recent</p>
+            <div className="recent-entry">
+              <img src={assets.message_icon} alt="Message Icon" />
+              <p>Sample query... </p>
+            </div>
+          </div>
+        ) : null}
+      </div>
+      <div className="bottom">
+        <div className="bottom-item recent-entry">
+          <img src={assets.question_icon} alt="Question" />
+          {extended ? <p>Help</p> : null}
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.history_icon} alt="History" />
+          {extended ? <p>Activity</p> : null}
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.settings_icon} alt="Settings" />
+          {extended ? <p>Settings</p> : null}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Sidebar;
